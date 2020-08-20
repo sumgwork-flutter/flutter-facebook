e4e4c2ff3efa4ab4565ca6a0c23f5a4b80f247b8
Sumit Govil <sumit.govil@optus.com.au>
2020-08-20 13:42:21 +1000

    Post Stats

--- lib/widgets/post_container.dart
+++ lib/widgets/post_container.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_facebook_responsive_ui/models/models.dart';
 import 'package:flutter_facebook_responsive_ui/widgets/widgets.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_facebook_responsive_ui/config/palette.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class PostContainer extends StatelessWidget {
   final Post post;
@@ -42,6 +44,10 @@ class PostContainer extends StatelessWidget {
                   ),
                 )
               : SizedBox.shrink(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 12.0),
+            child: _PostStats(post: post),
+          )
         ],
       ),
     );
@@ -95,6 +101,126 @@ class _PostHeader extends StatelessWidget {
           onPressed: () => print('More'),
         )
       ],
+    );
+  }
+}
+
+class _PostStats extends StatelessWidget {
+  final Post post;
+
+  _PostStats({this.post});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Palette.facebookBlue,
+                shape: BoxShape.circle,
+              ),
+              padding: EdgeInsets.all(
+                4.0,
+              ),
+              child: Icon(
+                Icons.thumb_up,
+                size: 10.0,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(width: 4.0),
+            Expanded(
+              child: Text(
+                '${post.likes}',
+                style: TextStyle(
+                  color: Colors.grey[600],
+                ),
+              ),
+            ),
+            Text(
+              '${post.comments} Comments',
+              style: TextStyle(
+                color: Colors.grey[600],
+              ),
+            ),
+            SizedBox(
+              width: 8.0,
+            ),
+            Text(
+              '${post.shares} Shares',
+              style: TextStyle(
+                color: Colors.grey[600],
+              ),
+            ),
+          ],
+        ),
+        Divider(),
+        Row(
+          children: [
+            _PostButton(
+              icon: Icon(
+                MdiIcons.thumbUpOutline,
+                color: Colors.grey[600],
+                size: 20.0,
+              ),
+              label: 'Like',
+              onTap: () => print('Like'),
+            ),
+            _PostButton(
+              icon: Icon(
+                MdiIcons.commentOutline,
+                color: Colors.grey[600],
+                size: 20.0,
+              ),
+              label: 'Comment',
+              onTap: () => print('Comment'),
+            ),
+            _PostButton(
+              icon: Icon(
+                MdiIcons.shareOutline,
+                color: Colors.grey[600],
+                size: 25.0,
+              ),
+              label: 'Share',
+              onTap: () => print('Share'),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class _PostButton extends StatelessWidget {
+  final Icon icon;
+  final String label;
+  final Function onTap;
+
+  _PostButton(
+      {@required this.icon, @required this.label, @required this.onTap});
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Material(
+        color: Colors.white,
+        child: InkWell(
+          onTap: onTap,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 12.0),
+            height: 25.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                icon,
+                SizedBox(width: 4.0),
+                Text(label),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
